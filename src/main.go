433ea90c1e23298b540af57d75c95f1d37b1f7ab
Vero <facu2000veronelli@outlook.com>
2021-11-11 10:58:42 -0300

    variables

--- src/main.go
+++ src/main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	//Declaracion de constantes
+	const pi float64 = 3.24
+	const pi2 = 3.54
+
+	fmt.Println("pi ", pi)
+	fmt.Println("pi2 ", pi2)
+
+	//Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Calcular un area de un cuadrado
+	const baseCuadrado int = 10
+	const alturaCuadrado int = 20
+	fmt.Println(baseCuadrado * alturaCuadrado)
 }
